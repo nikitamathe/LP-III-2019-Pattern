9d13fd484e12865cdc8dcad3a36dbc302a4a9e70
nikita <nikitamathe12@gmail.com>
2024-06-08 22:57:38 +0530

    DAA

--- DAA python/ass1.py
+++ DAA python/ass1.py
@@ -1,13 +1,19 @@
+# Fibonacci Sequence
+
 def recur_fibo(n):  
    if n <= 1:  
        return n  
    else:  
        return(recur_fibo(n-1) + recur_fibo(n-2))  
-# take input from the user  
+   
+# take input from the user. 
+ 
 nterms = int(input("How many terms? "))  
-# check if the number of terms is valid  
+
+# check if the number of terms is valid. 
+
 if nterms <= 0:  
-   print("Plese enter a positive integer")  
+   print("Please enter a positive integer")  
 else:  
    print("Fibonacci sequence:")  
    for i in range(nterms):  
